4f90852d8b14bed5ce984b110f0226399c4fb8af
Sasha1358 <0551553733z@gmail.com>
2023-10-30 22:54:16 -1200

    Добавьте файлы проекта.

---
+++ UnitTest6.1rek/UnitTest6.1rek.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab6.1pek/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest61rek
+{
+	TEST_CLASS(UnitTest61rek)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a[] = { 1, 3, 5, 7, 9 };
+			int size = 5;
+			Assert::AreEqual(Sum(a, size, 0), 0, 1e-1);
+		}
+	};
+}
